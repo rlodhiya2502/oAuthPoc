a5abf75c9e618ad0fb8d150632fe4375b08042cb
rlodhiya <rlodhiya2502@gmail.com>
2025-02-20 09:44:18 +0000

    Add initial project files including HTML structure, CSS, and configuration for Google and Apple OAuth

---
+++ assets/js/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    google: {
+        clientID: 'YOUR_GOOGLE_CLIENT_ID',
+        clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+        callbackURL: 'YOUR_GOOGLE_CALLBACK_URL'
+    },
+    apple: {
+        clientID: 'YOUR_APPLE_CLIENT_ID',
+        teamID: 'YOUR_APPLE_TEAM_ID',
+        keyID: 'YOUR_APPLE_KEY_ID',
+        privateKey: 'YOUR_APPLE_PRIVATE_KEY',
+        callbackURL: 'YOUR_APPLE_CALLBACK_URL'
+    }
+};
